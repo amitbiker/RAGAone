7840146c1484658974b226f2bbbbf39e7becdef5
amitbiker007 <amitbiker007@gmail.com>
2015-02-05 23:16:54 +0530

    commit

--- TestGitSample/src/com/test/git/sample/MainActivity.java
+++ TestGitSample/src/com/test/git/sample/MainActivity.java
@@ -2,18 +2,24 @@ package com.test.git.sample;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity 
+{
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Log.e("Hi...", "First Commit...");
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu) 
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return false;
